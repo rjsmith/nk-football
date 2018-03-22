a7c13afe79b96344b07def0bafa10e3f86a9faa2
Richard Smith <rsmith@rsbatechnology.co.uk>
2018-03-22 10:21:04 +0000

    Removed PhysicalResource file

--- modules/urn.uk.co.rsbatechnology.football.matches/src/PhysicalResource.java
+++
@@ -1,8 +0,0 @@
-
-public class PhysicalResource {
-
-	public static void main(String[] args) throws ClassNotFoundException {
-		Class<?> clazz = Class.forName("PhysicalResource",true, Thread.currentThread().getContextClassLoader());
-	}
-	
-}
